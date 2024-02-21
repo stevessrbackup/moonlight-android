07df6c9b9dc84532876c6d6b10369747b899bb32
wg <1205854813@qq.com>
2024-02-21 13:40:54 +0800

    修复switch按键一直触发的bug

--- app/src/main/java/com/limelight/binding/input/advance_setting/DigitalSwitch.java
+++ app/src/main/java/com/limelight/binding/input/advance_setting/DigitalSwitch.java
@@ -58,6 +58,7 @@ public class DigitalSwitch extends Element {
                         }
                     }
                 });
+                break;
             }
             case "m":{
                 addDigitalSwitchListener(new DigitalSwitch.DigitalSwitchListener() {
@@ -70,6 +71,7 @@ public class DigitalSwitch extends Element {
                         }
                     }
                 });
+                break;
             }
         }
 
